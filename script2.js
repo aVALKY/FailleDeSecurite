c20e1968584fbd57b8a1fa5fd071a5b7821c39c8
Timothé <timothegauquelin@gmail.com>
2024-04-09 19:05:51 +0200

    Correction du Script 2

--- script2.js
+++ script2.js
@@ -1,9 +1,10 @@
 function login() {
-    var pseudo = document.forms["login"]["pseudo"].value;
-    var password = document.forms["login"]["password"].value;
-    if (pseudo == "admin1@root.com" && password == "passadmin") {
-        window.location.href = "PageSecret3.html";
-    } else {
-        alert("Mauvais nom d'utilisateur ou mot de passe");
-    }
+  console.log("Click");
+  var pseudoValue = document.getElementById("pseudo_input").value;
+  var passwordValue = document.getElementById("password_input").value;
+  if (pseudoValue == "admin1@root.com" && passwordValue == "passadmin") {
+    window.location.href = "PageSecret3.html";
+  } else {
+    alert("Mauvais nom d'utilisateur ou mot de passe");
+  }
 }
